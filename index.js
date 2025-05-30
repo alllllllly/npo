744db1e59d4f60ba77c3541786e8bc7b936b011e
alllllllly <ariha.ichigo@gmail.com>
2025-05-30 23:05:40 +0900

    index.jsにpolarisからもらったコードをコピペ&書き換え

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+    console.log(`ログインしました: ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    const content = message.content.trim();
+    const allowedChannelId = "1370774836359467119"; // ここのIDを変更する、もしくはこの行を削除する(全チャンネルで有効化)
+    if (content.endsWith("い") && message.channel.id === allowedChannelId) {
+        message.channel.send("んぽ");
+    }
+});
+client.login('DISCORD_TOKEN');
