aad87c79a08d15c17baa1f20abd0c963a9ece032
alllllllly <ariha.ichigo@gmail.com>
2025-06-22 12:24:38 +0900

    プロ甲用チャンネルで返答するように更新

--- index.js
+++ index.js
@@ -9,13 +9,47 @@ client.once('ready', () => {
     console.log(`ログインしました: ${client.user.tag}`);
 });
 
+/*
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+client.once(Events.ClientReady, async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'コマンド実行中にエラーが発生しました。', ephemeral: true });
+  }
+});
+*/
+
 client.on('messageCreate', message => {
     if (message.author.bot) return;
 
     const content = message.content.trim();
-    const allowedChannelId = "1370774836359467119";
-    if (content.endsWith("い") && message.channel.id === allowedChannelId) {
+    const allowedChannelIds = ["1370774836359467119","1380757739965251654"];
+    if (allowedChannelIds.includes(message.channel.id)) {
+      if (content.endsWith("い")) {
         message.channel.send("んぽ");
+      }
     }
 });
 client.login(process.env.DISCORD_TOKEN);
